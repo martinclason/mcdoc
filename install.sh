6d2c5467d19563f0f4cb36f5df6f7ed754b17e2c
Martin Clason <martin@clason.se>
2019-09-20 22:00:24 +0200

    Switched to sh in install script

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
-#!/usr/local/bin/fish
-
-function try_link
-  if ! test -f $argv[2]
-    ln -s $PWD/$argv[1] $argv[2]
-    echo "link $argv[2] created for $argv[1]"
-  else
-    echo "$argv[2] already exists. Please fix this manually."
-  end
-end
-
+#!/bin/sh
 
 echo "Installing mcdoc commands..."
 
-try_link md2pdf.sh /usr/local/bin/md2pdf
+ln -s $PWD/md2pdf.sh /usr/local/bin/md2pdf
